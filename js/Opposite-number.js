a09a98ae398b034bac043a50fcec9a225a51b925
abdallah274 <aly0088152@gmail.com>
2025-12-22 21:47:40 +0200

    Opposite number

---
+++ js/Opposite-number.js
@@ -0,0 +1,20 @@
+//Very simple, given a number, find its opposite (additive inverse).
+// 1 >>> -1
+/* 
+function opposite(number) {
+  if (number > 0) {
+    number = - number
+  } else if (number < 0) {
+    number = Math.abs(number)
+    
+  } 
+  return number
+}
+  */
+///best Solutions
+/*
+function opposite(number) {
+  return number * -1;
+}
+*/
+
