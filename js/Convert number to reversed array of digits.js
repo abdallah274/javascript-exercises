c3aa8c6bca5b47a1ed769beccf5343f616c5c997
abdallah274 <aly0088152@gmail.com>
2025-12-22 20:03:07 +0200

    Convert-number-to-reversed-array

---
+++ js/Convert number to reversed array of digits.js
@@ -0,0 +1,18 @@
+//Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+ //35231 => [1,3,2,5,3] 
+
+ //change the number to string
+ //split the string an array
+ //chang the string to number 
+ //reverse the array
+
+
+
+function digitize(n) {
+
+return n.toString().split("").map((m) => Number(m)).reverse();
+}
+console.log(digitize(123456));
+
+
