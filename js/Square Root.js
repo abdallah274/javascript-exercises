a224e5a222f5535c3eb5beab5172bc9e06132e11
abdallah274 <aly0088152@gmail.com>
2026-01-11 19:48:47 +0200

    square root

---
+++ js/Square Root.js
@@ -0,0 +1,33 @@
+// Write a method, that will get an integer array as parameter and will process every number from this array.
+
+// Return a new array with processing every number of the input-array like this:
+
+// If the number has an integer square root, take this, otherwise square the number.
+
+
+//[4,3,9,7,2,1] -> [2,9,3,49,4,1]
+
+
+
+function squareOrSquareRoot(array) {
+// let newArray = [];
+// for (let i = 0; i < array.length; i++) {
+// if (Number.isInteger(Math.sqrt(array[i]))) {
+//  newArray.push(Math.sqrt(array[i]))
+  
+// }else {
+//   newArray.push(array[i] * array[i])
+// }
+// }
+
+
+ // return array.map((num) => Number.isInteger(Math.sqrt(num)) ? Math.sqrt(num):num * num) ;  
+
+
+}
+
+
+
+
+
+// console.log(squareOrSquareRoot([2,9,3,49,4,1]));
