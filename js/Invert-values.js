5ddb8b87ac840ec1ea24c3013f943c8ec8d31417
abdallah274 <aly0088152@gmail.com>
2026-01-14 18:08:20 +0200

    invert values

---
+++ js/Invert-values.js
@@ -0,0 +1,20 @@
+//Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+
+//[1, 2, 3, 4, 5] --> [-1, -2, -3, -4, -5]
+/*
+function invert(array) {
+   // the first solution
+   let arr=[];
+   for (let i = 0; i < array.length; i++) {
+   arr.push(-array[i])
+    
+   }
+   return arr;
+
+// the secound solution
+ return arr = array.map((num) => num * -1);
+
+}
+
+console.log(invert([-1, 2, -3, 4, 5]));
+*/
